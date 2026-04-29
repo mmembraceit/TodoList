5e378c12a94718258263fd43b3c104a3567cc9cb
mmembraceit <mmb@embrace-it.com>
2026-04-29 11:37:12 +0200

    feat(presentation): chore files changes

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddApplication();
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
