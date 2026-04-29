de1b0fb6dd50fbaec999d8d3b36a589f99565bc8
mmembraceit <mmb@embrace-it.com>
2026-04-29 11:35:10 +0200

    feat(infra): dependency injection

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,12 +1,28 @@
+using Infrastructure.Persistence;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configuration);
+
+        var connectionString = configuration.GetConnectionString("TodoListDatabase");
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException("Connection string 'TodoListDatabase' was not found.");
+        }
+
+        services.AddDbContext<TodoListDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
 
         return services;
     }
